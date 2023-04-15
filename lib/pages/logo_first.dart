a491bf105aacfd80c9699818d71b3ccd05784355
Tanya Lebedeva <tanuceerveevna@gmail.com>
2023-04-15 07:43:34 +0300

    first loggin screen added

---
+++ lib/pages/logo_first.dart
@@ -0,0 +1,70 @@
+
+
+import 'package:flutter/material.dart';
+
+
+class Login_firstScreen extends StatefulWidget {
+
+
+
+  Login_firstScreen({Key? key}) : super(key: key);
+
+
+  @override
+  State<Login_firstScreen> createState() => _Login_firstScreenState();
+}
+
+class _Login_firstScreenState extends State<Login_firstScreen> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffF5F5F5),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/logoVizitka.png'),
+            SizedBox(height: 16),
+            Text('InFly',
+            style: TextStyle(
+              fontSize: 35,
+              color: Color(0xff355A84),
+              fontWeight: FontWeight.w600
+            ),),
+            SizedBox(height: 36),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: TextField(
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Color(0xff9CA9BA),
+                    border:  OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 0,
+                        style: BorderStyle.none,
+                      ),
+                    ),
+                  hintText: 'Email',
+                  hintStyle: TextStyle(color: Color(0xff000000))
+                ),
+              ),
+            ),
+            SizedBox(height: 152),
+            TextButton(
+              onPressed: (){},
+              child: Text('Продолжить',
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Color(0xff355A84),
+                  fontWeight: FontWeight.w600
+                ),),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
